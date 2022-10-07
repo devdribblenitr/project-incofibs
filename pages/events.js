44ace42b28a36698de1ada96fe701f89cfc4480e
Ayush Shaw <ayushshaw127@gmail.com>
2022-10-07 16:13:36 +0530

    added dummy image of event and did some styling

--- pages/events.js
+++ pages/events.js
@@ -4,7 +4,7 @@ export default function Events(){
     return(
         <div className="bg-gray-100 px-2">
             <h1 className="text-6xl font-semibold text-center py-6">Events</h1>
-            <h2 className="text-4xl font-semibold">Upcoming events</h2>
+            <h2 className="text-4xl font-semibold bg-sky-500 py-4 px-2 rounded-lg text-white">Upcoming events</h2>
             <div className="py-6">
                 <EventCard></EventCard>
             </div>
