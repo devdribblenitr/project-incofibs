8b40850fe418b50e960b9e739f1d72bc55bcc3e4
Ayush Shaw <ayushshaw127@gmail.com>
2022-10-07 23:38:28 +0530

    modified styles

--- components/EventCard.jsx
+++ components/EventCard.jsx
@@ -5,8 +5,8 @@ import KeynoteSpeakers from './KeynoteSpeakers';
 export default function EventCard() {
     return (
         <>
-        <div className='grid grid-cols-1 md:grid-cols-2'>
-            <div className='flex flex-col justify-center'>
+        <div className='grid grid-cols-1 md:grid-cols-2 bg-gray-200 rounded-lg'>
+            <div className='flex flex-col justify-center px-4'>
                 <div>
                     <h3 className="text-3xl font-semibold text-blue-800">Conference on Artificial Intelligence</h3>
                     <p className="py-4 text-gray-500">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias, aperiam perferendis modi dolorem facere laudantium quas officia, atque sed eaque quis, nam nemo! Ipsam voluptatem dolores consequatur eius. Asperiores, nemo!</p>
@@ -30,8 +30,8 @@ export default function EventCard() {
                     </ul>
                 </div>
             </div>
-            <div className='mx-auto w-fit py-8'>
-                <img src="https://images.pexels.com/photos/12303537/pexels-photo-12303537.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" style={{height: "400px"}} />
+            <div className='mx-auto py-8 md:py-0 md:mr-0'>
+                <img src="https://images.pexels.com/photos/12303537/pexels-photo-12303537.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" style={{height: "400px"}} className="rounded-r-xl" />
             </div>
         </div>
         <div>
