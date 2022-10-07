5deda288bad23fc148e569b884f22ea6d9f52a23
Ayush Shaw <ayushshaw127@gmail.com>
2022-10-07 22:55:16 +0530

    Created keynote speakers

--- components/EventCard.jsx
+++ components/EventCard.jsx
@@ -1,16 +1,18 @@
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
+import KeynoteSpeakers from './KeynoteSpeakers';
 
 export default function EventCard() {
     return (
+        <>
         <div className='grid grid-cols-1 md:grid-cols-2'>
-            <div>
+            <div className='flex flex-col justify-center'>
                 <div>
-                    <h3 className="text-3xl font-semibold text-sky-600">Conference on Artificial Intelligence</h3>
+                    <h3 className="text-3xl font-semibold text-blue-800">Conference on Artificial Intelligence</h3>
                     <p className="py-4 text-gray-500">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias, aperiam perferendis modi dolorem facere laudantium quas officia, atque sed eaque quis, nam nemo! Ipsam voluptatem dolores consequatur eius. Asperiores, nemo!</p>
                 </div>
                 <div>
-                    <ul className="flex flex-col w-fit">
+                    <ul className="flex flex-col md:w-fit">
                         <li className="px-2 py-2 md:pl-0">
                             <div className="bg-sky-500 py-2 text-white rounded-md px-4">
                                 <CalendarMonthIcon></CalendarMonthIcon>
@@ -28,9 +30,13 @@ export default function EventCard() {
                     </ul>
                 </div>
             </div>
-            <div className='mx-auto w-fit'>
-                <img src="https://images.pexels.com/photos/12303537/pexels-photo-12303537.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" style={{height: "360px"}} />
+            <div className='mx-auto w-fit py-8'>
+                <img src="https://images.pexels.com/photos/12303537/pexels-photo-12303537.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" style={{height: "400px"}} />
             </div>
         </div>
+        <div>
+            <KeynoteSpeakers></KeynoteSpeakers>
+        </div>
+        </>
     )
 }
--- pages/events.js
+++ pages/events.js
@@ -3,10 +3,13 @@ import EventCard from "../components/EventCard"
 export default function Events(){
     return(
         <div className="bg-gray-100 px-2">
+            <div className="container mx-auto">
             <h1 className="text-6xl font-semibold text-center py-6">Events</h1>
-            <h2 className="text-4xl font-semibold bg-sky-500 py-4 px-2 rounded-lg text-white">Upcoming events</h2>
-            <div className="py-6">
+            <h2 className="text-4xl font-semibold bg-sky-500 py-4 px-2 rounded-lg text-white">Upcoming events
+            </h2>
+            <div className="py-6 mx-auto" style={{maxWidth: "900px"}}>
                 <EventCard></EventCard>
+            </div>
             </div>
         </div>
     )
