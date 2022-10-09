4b0b7cf00dadf06d936e9c614bc0fa21bd28bc9d
Ayush Shaw <ayushshaw127@gmail.com>
2022-10-09 16:02:09 +0530

    changed layout of past event

--- components/EventCard.jsx
+++ components/EventCard.jsx
@@ -7,7 +7,7 @@ export default function EventCard() {
         <>
         <div className='grid grid-cols-1 md:grid-cols-2 bg-gray-200 rounded-lg'>
             <div className='flex flex-col justify-center px-4'>
-                <div>
+                <div className='py-4 md:py-0'>
                     <h3 className="text-3xl font-semibold text-blue-800">Conference on Artificial Intelligence</h3>
                     <p className="py-4 text-gray-500">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias, aperiam perferendis modi dolorem facere laudantium quas officia, atque sed eaque quis, nam nemo! Ipsam voluptatem dolores consequatur eius. Asperiores, nemo!</p>
                 </div>
@@ -31,7 +31,7 @@ export default function EventCard() {
                 </div>
             </div>
             <div className='mx-auto py-8 md:py-0 md:mr-0'>
-                <img src="https://images.pexels.com/photos/12303537/pexels-photo-12303537.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" style={{height: "400px"}} className="rounded-r-xl" />
+                <img src="https://images.pexels.com/photos/12303537/pexels-photo-12303537.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" style={{height: "400px"}} className="md:rounded-r-xl" />
             </div>
         </div>
         <div>
