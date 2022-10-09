4cf8c9a94a16951dc4aba5e07c347d2e6044a713
lalitku-sahu <97198304+lalitku-sahu@users.noreply.github.com>
2022-10-09 20:27:07 +0530

    footer code

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Navbar from '../components/Navbar'
 import styles from '../styles/Home.module.css' 
-import Footer from './Footer'
 
 export default function Home() {
   return (
