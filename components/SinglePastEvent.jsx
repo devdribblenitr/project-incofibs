1ab1fbf9d80a12c189516a92650d8bf2290f2c9b
Ayush Shaw <ayushshaw127@gmail.com>
2022-10-09 16:31:30 +0530

    made past event responsive

--- components/SinglePastEvent.jsx
+++ components/SinglePastEvent.jsx
@@ -3,19 +3,19 @@ import React from 'react'
 function SinglePastEvent() {
     return (
         <div className='py-2'>
-            <div className='flex bg-slate-200 justify-between rounded-md'>
-                <div className='flex'>
-                    <div className='p-4'>
+            <div className='md:flex bg-slate-200 justify-between rounded-md p-2'>
+                <div className='flex flex-wrap'>
+                    <div className='p-4 w-fit h-fit'>
                         <img src="https://images.pexels.com/photos/12303537/pexels-photo-12303537.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" className='w-16 rounded-full h-16' />
                     </div>
                     <div className='h-fit my-auto'>
                         <h2 className='font-semibold text-lg text-blue-700'>Conference on IOT</h2>
-                        <p>A conference on the applications of IOT...</p>
+                        <p>A conference on the applications of IOT11...</p>
                     </div>
                 </div>
-                <div className='h-fit my-auto px-4'>
+                <div className='h-fit my-auto px-4 w-fit ml-auto'>
                     <h2 className='text-lg text-sky-500 font-semibold'>BBA Hall</h2>
-                    <h2 className='text-sm text-gray-400'>14/10/2022</h2>
+                    <h2 className='text-sm text-gray-400'>02/10/2022</h2>
                 </div>
             </div>
         </div>
