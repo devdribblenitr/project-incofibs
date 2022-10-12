67fcdd86fe98885f4a0eb97d88b1cc8169bd1286
Ayush Shaw <ayushshaw127@gmail.com>
2022-10-12 14:36:19 +0530

    added image and styling to important dates

--- pages/temp.js
+++ pages/temp.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ImportantDate from '../components/ImportantDate'
 export default function temp() {
   return (
-    <div className='bg-green-200 mx-auto' style={{maxWidth: "900px"}}>
+    <div className='bg-green-200'>
       <ImportantDate></ImportantDate>
     </div>
   )
