06ca837ad7e1f03ec222f9b00b75fa5a89e8e031
lalitku-sahu <97198304+lalitku-sahu@users.noreply.github.com>
2022-10-13 14:13:43 +0530

    revised code

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,70 +1,86 @@
 
-const Footer = () => {
+function Footer() {
   return (
     <footer>
-    <div className="container">
-      <div className="row">
-      <div className="col-12 mx-auto">
-      <div className="row">
-      <div className="col-6 mx-auto">
-        <div class="card"> 
-            <h2>ABOUT US</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit iusto laborum soluta. Numquam non dignissimos quasi dolor autem, delectus voluptate ea optio quae architecto aspernatur voluptatibus adipisci tempora consectetur voluptatum!</p>  
-            <div className="row">
-              <div className="col-10 mx-auto">
+      <div className="container">
+        <div className="row">
+          <div className="flex space-x-40">
+            <div className="grid grid-cols-3 gap-14 place-items-center content-evenly 
+              pt-16 pr-16 pl-36">
+              <div className="card">
+                <h2 className="pt-6 pl-14">ABOUT US</h2>
+                <p className="pt-10 pl-14">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit iusto laborum soluta. Numquam non dignissimos quasi dolor autem, delectus voluptate ea optio quae architecto aspernatur voluptatibus adipisci tempora consectetur voluptatum!</p>
+                <div >
+                  <div >
+                    <div className="grid grid-cols-5 gap-2 pl-14 pt-24 pr-14" >
+                      <div className="social-links">
+                        <a href=""><i class="fa-brands fa-twitter"></i></a>
+                      </div>
+                      <div className="social-links">
+                        <a href=""><i class="fa-brands fa-linkedin-in"></i></a>
+                      </div>
+                      <div className="social-links">
+                        <a href=""><i class="fa-brands fa-facebook-f"></i></a>
+                      </div>
+                      <div className="social-links">
+                        <a href=""><i class="fa-brands fa-instagram"></i></a>
+                      </div>
+                      <div className="social-links">
+                        <a href=""><i class="fa-solid fa-phone"></i></a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="card">
+                <h2 className="pt-6 pl-14">ABOUT US</h2>
+                <p className="pt-10 pl-14">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit iusto laborum soluta. Numquam non dignissimos quasi dolor autem, delectus voluptate ea optio quae architecto aspernatur voluptatibus adipisci tempora consectetur voluptatum!</p>
+                <div >
+                  <div >
+                    <div className="grid grid-cols-5 gap-2 pl-14 pt-24 pr-14">
+                      <div className="social-links">
+                        <a href=""><i class="fa-brands fa-twitter"></i></a>
+                      </div>
+                      <div className="social-links">
+                        <a href=""><i class="fa-brands fa-linkedin-in"></i></a>
+                      </div>
+                      <div className="social-links">
+                        <a href=""><i class="fa-brands fa-facebook-f"></i></a>
+                      </div>
+                      <div className="social-links">
+                        <a href=""><i class="fa-brands fa-instagram"></i></a>
+                      </div>
+                      <div className="social-links">
+                        <a href=""><i class="fa-solid fa-phone"></i></a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="card">
+                <h1 className="pt-6">InCoFIBS 2k23 </h1>
+                <h1 className="pt-7">LIFE SCIENCE Dept</h1>
+                <br />
+                <div className="pl-12 pt-2"><hr align="center" width="85%" /></div>
                 <div className="row">
-                  <div className="col-2 mx-auto">
-                    <i class="fab fa-twitter social-links"></i>
-                  </div>
-                  <div className="col-2 mx-auto">
-                    <i class="fab fa-linkedin-in social-links"></i>
-                  </div>
-                  <div className="col-2 mx-auto">
-                    <i class="fab fa-facebook-f social-links"></i>
-                  </div>
-                  <div className="col-2 mx-auto">
-                    <i class="fab fa-instagram social-links"></i>
-                  </div>
-                  <div className="col-2 mx-auto">
-                    <i class="fab fa-whatsapp social-links"></i>
+                  <div className="grid grid-cols-3 gap-4 place-items-center content-evenly 
+            pt-6 pr-6 pl-6">
+                    <img src="diamond.jpeg" />
+                    <img src="diamond.jpeg" />
+                    <img src="logo.jpeg" />
                   </div>
                 </div>
               </div>
             </div>
+          </div>
+          <div className="flex space-y-40">
+            <div className="base-box pt-5 place-items-center ">
+              <h3><p>@2023 Made with &#129505; by ABC Tech Team</p></h3>
+            </div>
+          </div>
         </div>
       </div>
-      <div className="col-6">
-        <div class="card"> 
-            <h2>ABOUT US</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit iusto laborum soluta. Numquam non dignissimos quasi dolor autem, delectus voluptate ea optio quae architecto aspernatur voluptatibus adipisci tempora consectetur voluptatum!</p>  
-            <div className="row">
-              <div className="col-10 mx-auto">
-                <div className="row">
-                  <div className="col-2 mx-auto">
-                    <i class="fab fa-twitter social-links"></i>
-                  </div>
-                  <div className="col-2 mx-auto">
-                    <i class="fab fa-linkedin-in social-links"></i>
-                  </div>
-                  <div className="col-2 mx-auto">
-                    <i class="fab fa-facebook-f social-links"></i>
-                  </div>
-                  <div className="col-2 mx-auto">
-                    <i class="fab fa-instagram social-links"></i>
-                  </div>
-                  <div className="col-2 mx-auto">
-                    <i class="fab fa-whatsapp social-links"></i>
-                  </div>
-                </div>
-              </div>
-            </div> 
-        </div>
-      </div>
-    </div>
-    </div>
-    </div>
-    </div>
-</footer>
+    </footer >
   )
 }
 
