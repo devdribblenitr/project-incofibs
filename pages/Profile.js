3fdcb27d0ade54a37df6b6296b11ff2f61847e28
sandeep kumar mahato <sandeepkumarmahato712@gmail.com>
2022-10-15 01:38:56 +0530

    init

--- pages/Profile.js
+++ pages/Profile.js
@@ -1,7 +1,5 @@
 import React from "react";
-import Image from "next/image";
 import Link from "next/link";
-import profilePic from "../public/profile.png";
 
 export default function Profile() {
   return (
