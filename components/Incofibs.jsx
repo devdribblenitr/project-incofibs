3a9fee3b4db99b3e27703e24a9daba4e504a969f
Anubhuti Pandey <anubhutipandey028@gmail.com>
2022-10-15 11:20:38 +0530

    pushing about us fr improvement

--- components/Incofibs.jsx
+++ components/Incofibs.jsx
@@ -1,23 +1,28 @@
 import React from 'react';
+import gig from './gig.css';
+
 const Incofibs = () => {
-    return ( <>
-        
-        <div className='mb-20 flex flex-col items-center lg:my-10'>
-        <div className='mx-4 '>
-        <div className='flex flex-col items-center'>
-        <img className='center z-10 w-[400px] lg:ml-[400px] lg:absolute h-72 lg:h-96 rounded-3xl shadow-[0_35px_60px_-15px_rgba(0,0,0.9,0.9)]' src="https://res.cloudinary.com/dzbax077h/image/upload/v1665169731/conf_r2ahdf.jpg" alt='incofibs'/>  
+    return ( 
+    <>
+    <div className="mt-28 ml-16 sm:ml-0  flex flex-col sm:flex-row justify-center items-center space-default space-y-10 space-x-10">
+    {/* <div className="diamond-content"> */}
+        <div class="diamond-shape rounded-3xl h-[250px] w-[250px]">
+            
+            <div class="bg"><img className="h-[250px] w-[250px]" src="https://res.cloudinary.com/dzbax077h/image/upload/v1665689632/WhatsApp_Image_2022-10-14_at_1.03.32_AM_sgmtyi.jpg" class="img-fluid"/></div>
         </div>
-        <div className="rounded-3xl mt-10 mb-4 lg:mr-[400px] m-20 lg:mb-20">
-        <div className="flex flex-col space-y-4 bg-slate-100 p-5 py-20 lg:pl-20 lg:pr-44 text-center text-2xl rounded-3xl h-fit max-w-5xl shadow-2xl">
-        <div className='mx-4 bg-white font-serif text-6xl text-bold text-center lg:pr-20'>About InCoFibs</div><div>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse incidunt repudiandae eos temporibus voluptate quibusdam animi nesciunt illo consequuntur enim, aspernatur ab labore hic. Ad a fugit iure sit in?</div>
-        <div>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae accusamus quisquam rem cupiditate quos a omnis fugit fuga! Officiis nulla quod omnis, suscipit maiores unde.</div>
-        <div>Lorem ipsum dolor, sit amet consectetur adipisicing elit. fugit fuga! Officiis nulla quod omnis, suscipit maiores unde.</div>
-        
-        </div>
-        </div>
-        </div>
-        </div>
-        </>
+    {/* </div> */}
+    <div className='bg-blue-900 h-[400px] w-[400px] rounded-3xl -rotate-45 z-10 '>
+        <h1 className='text-3xl font-serif text-red-500 text-center rotate-45 mt-8 ml-56'>ABOUT CONFERENCE</h1>
+        <p className='text-center text-white text-lg rotate-45 pr-4 mt-3'> elit. Laborum id laboriosam fuga sit. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eveniet id nam accusantium molestiae quasi. Voluptate animi aliquam odio magnam voluptates et! Eum labore nesciunt facere illo ea. Commodi, eius repellat?</p>
+    </div>
+    {/* <div className="diamond-content"> */}
+    <div class="diamond-shape rounded-3xl h-[250px] w-[250px]">
+        <div class="bg"><img className="h-[250px] w-[250px]" src="https://res.cloudinary.com/dzbax077h/image/upload/v1665689632/WhatsApp_Image_2022-10-14_at_1.03.32_AM_sgmtyi.jpg" class="img-fluid"/></div>
+    </div>
+    {/* </div> */}
+    </div>
+   
+    </>
      );
 }
  
