1cbdeffe88e5aedd6b400d198f46080c93ef9d5a
Mohammad Sahil <sahilmejakhas@gmail.com>
2022-10-16 15:12:17 +0530

    package modified

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import '../styles/globals.css'
 //import '../styles/gallery.css'
-// import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import { SessionProvider } from "next-auth/react"
 
 export default function MyApp({
