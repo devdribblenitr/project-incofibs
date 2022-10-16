935b01eed18859305c8ada5b5bc31079868f999b
Debaditya <ddev2636@gmail.com>
2022-10-16 14:33:41 +0530

    changes madein herosection

--- components/Countdownclock.js
+++ components/Countdownclock.js
@@ -45,14 +45,13 @@ const Countdownclock = () => {
       <div className="flex flex-col items-center justify-center text-white mb-4">
         <button className="bg-primary-60 rounded-tl-bye rounded-br-bye text-lg p-2 ">
           {" "}
-          20th January 2023
+          2nd February 2023
         </button>
         <div className="text-2xl lg:text-5xl mt-6 flex justify-center pl-4 sm:pl-0 mb-1">
-          HEADING HEADING
+          INCOFIBS  2k23
         </div>
         <div className="text-lg w-4/5 flex justify-center mb-1">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe,
-          ducimus.
+         International Conference on frontier in Biological Sciences
         </div>
       </div>
       <div className="flex justify-center w-screen">
@@ -74,7 +73,7 @@ const Countdownclock = () => {
           </div>
           <div className="text-sm uppercase  flex justify-center">hours</div>
         </div>
-        <div className="text-white bg-transparent text-xl sm:text-3xl lg:text-3xl ml-2 mr-2  md:ml-4  h-1/6 md:h-1/3  lg:p-6 xl:p-7 sm:p-2 sm:pl-3 sm:pr-3 lg:mr-6 sm:border-gray-100 sm:border-2 rounded-tl-br10 rounded-br-br10 lg:rounded-tl-br25 lg:rounded-br-br25">
+        <div className="text-white bg-transparent text-xl sm:text-3xl lg:text-3xl ml-2 mr-2  md:ml-4  h-1/6 md:h-1/3  lg:p-6 xl:p-7 sm:p-2 sm:pl-3 sm:pr-3 lg:mr-6 sm:border-gray-100 sm:border-2 rounded-bl-br10 rounded-tr-br10 lg:rounded-bl-br25 lg:rounded-tr-br25">
           <div className="flex flex-col items-center">
             <div className=" text-white flex justify-center text-md lg:text-xl xl:text-3xl font-bold mb-1">
               {padWithZeros(minutes, 2)}
@@ -83,7 +82,7 @@ const Countdownclock = () => {
           </div>
           <div className="text-sm uppercase  flex justify-center">minutes</div>
         </div>
-        <div className="text-white bg-transparent text-xl sm:text-3xl lg:text-3xl ml-2 mr-2  md:ml-4  h-1/6 md:h-1/3   lg:p-6  xl:p-7 sm:p-2 sm:pl-3 sm:pr-3 lg:mr-6 border-gray-100 sm:border-2 rounded-tl-br10 rounded-br-br10 lg:rounded-tl-br25 lg:rounded-br-br25">
+        <div className="text-white bg-transparent text-xl sm:text-3xl lg:text-3xl ml-2 mr-2  md:ml-4  h-1/6 md:h-1/3   lg:p-6  xl:p-7 sm:p-2 sm:pl-3 sm:pr-3 lg:mr-6 border-gray-100 sm:border-2 rounded-bl-br10 rounded-tr-br10 lg:rounded-bl-br25 lg:rounded-tr-br25">
           <div className="flex flex-col items-center">
             <div className=" text-white flex justify-center text-md lg:text-xl xl:text-3xl font-bold mb-1">
               {padWithZeros(seconds, 2)}
