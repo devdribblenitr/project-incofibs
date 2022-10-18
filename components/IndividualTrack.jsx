5ee96b0cfe4d1d10031b699205b749bc3b34d762
Ayush Shaw <61586233+ayushshaw90@users.noreply.github.com>
2022-10-18 14:07:17 +0530

    added rounded

--- components/IndividualTrack.jsx
+++ components/IndividualTrack.jsx
@@ -6,7 +6,7 @@ export default function IndividualTrack({ track }) {
     let index=0;
     return (
         <div className='px-4 py-4'>
-            <div style={{ backgroundColor: "#184353"}} className={`px-6 py-4 rounded-xl ${style.individualTrack}`}>
+            <div style={{ backgroundColor: "#184353"}} className={`px-6 py-4 ${style["rounded-xl"]} ${style.individualTrack}`}>
                 {
                     track.map((item) => {
                         let cn = `text-white py-4 text-xl px-4 hover:scale-105 duration-200 ease-in-out ${style.trackItem}`;
