64d56944166ffae643a3b502c7ffa99f30c0582b
Mohammad Sahil <sahilmejakhas@gmail.com>
2022-10-19 12:55:06 +0530

    corousel added

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import '../styles/gallery.css'
 import '../styles/style_registration.css'
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { SessionProvider } from "next-auth/react"
 
 export default function MyApp({
--- pages/herosection.js
+++ pages/herosection.js
@@ -10,30 +10,7 @@ import { Carousel } from 'react-responsive-carousel';
 const HeroSection = () => {
   return (
     <div className="">
-      <div className="fixed w-full">
-        {/* <Carousel controls={false} fade={true} indicators={false}>
-          <Carousel.Item interval={1000}>
-            <img
-              className="d-block w-100 h-screen lg:w-full lg:h-screen "
-              src="https://c0.wallpaperflare.com/preview/440/715/681/dark-green-green-elaf-dark-background.jpg"
-              alt="First slide"
-            />
-          </Carousel.Item>
-          <Carousel.Item interval={1000}>
-            <img
-              className="d-block w-100 h-screen lg:w-full lg:h-screen"
-              src="https://cdn.wallpapersafari.com/74/34/921CI3.jpg"
-              alt="Second slide"
-            />
-          </Carousel.Item>
-          <Carousel.Item interval={1000}>
-            <img
-              className="d-block w-100 h-screen lg:w-full lg:h-screen"
-              src="https://t3.ftcdn.net/jpg/02/97/91/38/360_F_297913876_pxLCVBrLEyznyqdHvNpKmNRPvYWoqlEW.jpg"
-              alt="Third slide"
-            />
-          </Carousel.Item>
-        </Carousel> */}
+      <div className="absolute w-full">
         <Carousel infiniteLoop={true} interval={3000} autoPlay={true}renderIndicator={false} axis='horizontal' showArrows={false} showIndicators={false} showStatus={false}>
                 <div>
                     <img src="https://c0.wallpaperflare.com/preview/440/715/681/dark-green-green-elaf-dark-background.jpg"
@@ -59,9 +36,9 @@ const HeroSection = () => {
         <div className="mt-14 mb-4 pb-2 flex items-end lg:mb-20 xl:pb-44 h-3/4">
           <Countdownclock />
           </div>
-          <div className="flex justify-center items-end  xl:mt-8 ">
+          {/* <div className="flex justify-center items-end  xl:mt-8 ">
           <Herosectioncard />
-        </div>
+        </div> */}
       </div>
     </div>
   );
