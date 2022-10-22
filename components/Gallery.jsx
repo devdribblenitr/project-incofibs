1587685ed20c7c752f1fb09602f2f491deaff92c
Subham Das <subhamdassneha@gmail.com>
2022-10-22 20:32:33 +0530

    gallery updated

--- components/Gallery.jsx
+++ components/Gallery.jsx
@@ -1,16 +1,83 @@
 import { StylesContext } from '@material-ui/styles'
+import Image from 'next/image'
 import React from 'react'
 
 import styles from '../styles/gallery.module.css'
 
+const images = [
+  {
+    name:'IMAGE',
+    desc: 'Image'
+  }
+]
+
 const Gallery = () => {
   return (
     <div className ={styles.gallery}>
-      <section>
-        <h1>Section 1</h1>
+          <h1>Section 1</h1>
+      <section className={styles.section} >
+    
+       
+        <div>
+          <img src="./IMAGE.png" alt="Alternate text" />
+        </div>
+        <div>
+          <img src="./IMAGE.png" alt="Alternate text" />
+        </div>
+        <div>
+          <img src="./IMAGE.png" alt="Alternate text" />
+        </div>
+        <div>
+          <img src="./IMAGE.png" alt="Alternate text" />
+        </div>
+        <div>
+          <img src="./IMAGE.png" alt="Alternate text" />
+        </div>
+        <div>
+          <img src="./IMAGE.png" alt="Alternate text" />
+        </div>
+        <div>
+          <img src="./IMAGE.png" alt="Alternate text" />
+        </div>
+        <div>
+          <img src="./IMAGE.png" alt="Alternate text" />
+        </div>
+        <div>
+          <img src="./IMAGE.png" alt="Alternate text" />
+        </div>
+        
       </section>
-      <section>
-        <h1>Section 2</h1>
+      <h1>Section 2</h1>
+      <section className={styles.section}>
+       
+     
+        <div>
+          <img src="./IMAGE.png" alt="Alternate text" />
+        </div>
+        <div>
+          <img src="./IMAGE.png" alt="Alternate text" />
+        </div>
+        <div>
+          <img src="./IMAGE.png" alt="Alternate text" />
+        </div>
+        <div>
+          <img src="./IMAGE.png" alt="Alternate text" />
+        </div>
+        <div>
+          <img src="./IMAGE.png" alt="Alternate text" />
+        </div>
+        <div>
+          <img src="./IMAGE.png" alt="Alternate text" />
+        </div>
+        <div>
+          <img src="./IMAGE.png" alt="Alternate text" />
+        </div>
+        <div>
+          <img src="./IMAGE.png" alt="Alternate text" />
+        </div>
+        <div>
+          <img src="./IMAGE.png" alt="Alternate text" />
+        </div>
       </section>
     </div>
   )
