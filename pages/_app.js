daf6eddd7e336a34a0f784da6452318ffec6a846
Subham Das <subhamdassneha@gmail.com>
2022-10-22 19:42:22 +0530

    Gallery

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import '../styles/globals.css'
-import '../styles/gallery.css'
-import '../styles/style_registration.css'
+
+
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import Navbar from '../components/Navbar'; // requires a loader
 import { SessionProvider } from "next-auth/react"
