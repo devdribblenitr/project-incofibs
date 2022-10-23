d867338a9d10a61a2f99c6d977ff16d7e9994a87
Ayush Shaw <61586233+ayushshaw90@users.noreply.github.com>
2022-10-23 15:05:02 +0530

    Added page background color and navbar to events

--- components/Speaker.js
+++ components/Speaker.js
@@ -1,9 +1,12 @@
+import styles from '../styles/Speaker.module.css'
+
 export default function Speaker({name, designation}){
     return(
-        <div className="py-2 mx-auto w-60">
+        <div className="py-2 mx-auto w-64">
             <div className="rounded-lg" style={{backgroundColor: "#022a36", borderRadius: "0.75rem"}}>
                 <div>
-                <img src="https://images.pexels.com/photos/6325954/pexels-photo-6325954.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" className=" rounded-t-lg" style={{borderTopLeftRadius: "0.75rem", borderTopRightRadius: "0.75rem"}} />
+                    <div className={`absolute ${styles.cardSpeak} h-48 w-64 duration-200 ease-in`} style={{ borderTopLeftRadius: "0.75rem", borderTopRightRadius: "0.75rem"}}></div>
+                <img src="https://images.pexels.com/photos/6325954/pexels-photo-6325954.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" className="rounded-t-lg h-48 w-64" style={{borderTopLeftRadius: "0.75rem", borderTopRightRadius: "0.75rem"}} />
                 </div>
                 <div className="text-center pt-3 pb-6 h-32">
                     <h2 className="text-xl font-semibold" style={{color: "rgb(232, 76, 61)"}}>{name}</h2>
--- pages/events.js
+++ pages/events.js
@@ -1,4 +1,5 @@
 import EventCard from "../components/EventCard"
+import Navbar from "../components/Navbar"
 import PastEvents from "../components/PastEvents"
 import PastKeynoteSpeakers from "../components/PastKeynoteSpeakers"
 
@@ -51,18 +52,24 @@ export default function Events() {
         }
     ]
     return (
-        <div className="bg-gray-200 px-2">
-            <div className=" mx-auto w-full lg:w-5/6">
+        <div className="bg-gray-200">
+            <Navbar />
+            <div className="px-2 lg:px-0 mx-auto w-full lg:w-5/6 md:py-6">
                 {/* <h1 className="text-6xl font-semibold text-center py-6">Events</h1>
                 <h2 className="text-4xl font-semibold bg-sky-500 py-4 px-2 rounded-lg text-white text-center">Upcoming events
                 </h2> */}
                 {/* <div className="py-6 mx-auto" style={{ maxWidth: "900px" }}>
                     <EventCard></EventCard>
                 </div> */}
-                
-                <PastKeynoteSpeakers heading="Keynote speakers" speakers={keynoteSpeakers}></PastKeynoteSpeakers>
+                <div className="bg-slate-100">
+                <PastKeynoteSpeakers heading="Keynote Speakers" speakers={keynoteSpeakers}></PastKeynoteSpeakers>
+                </div>
+                <div className="bg-slate-100">
                 <PastKeynoteSpeakers heading="Speakers and faculty" speakers={keynoteSpeakers}></PastKeynoteSpeakers>
+                </div>
+                <div className="bg-slate-100">
                 <PastEvents events={pastEvent}></PastEvents>
+                </div>
             </div>
         </div>
     )
