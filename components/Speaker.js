d2c98f1985c2008df606be6301317ec029685384
Ayush Shaw <61586233+ayushshaw90@users.noreply.github.com>
2022-10-23 15:06:41 +0530

    added a bit padding to speaker card

--- components/Speaker.js
+++ components/Speaker.js
@@ -8,7 +8,7 @@ export default function Speaker({name, designation}){
                     <div className={`absolute ${styles.cardSpeak} h-48 w-64 duration-200 ease-in`} style={{ borderTopLeftRadius: "0.75rem", borderTopRightRadius: "0.75rem"}}></div>
                 <img src="https://images.pexels.com/photos/6325954/pexels-photo-6325954.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" className="rounded-t-lg h-48 w-64" style={{borderTopLeftRadius: "0.75rem", borderTopRightRadius: "0.75rem"}} />
                 </div>
-                <div className="text-center pt-3 pb-6 h-32">
+                <div className="text-center pt-3 pb-6 h-32 px-1">
                     <h2 className="text-xl font-semibold" style={{color: "rgb(232, 76, 61)"}}>{name}</h2>
                     <p className="text-gray-300">{designation}</p>
                 </div>
