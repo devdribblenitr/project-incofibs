c1d11fe890525b479fc5221a8058c89f21ff5939
Aashirbad Sabat <aum.aashirbad@gmail.com>
2022-10-26 00:46:38 +0530

    contact page done

---
+++ components/ContactData.js
@@ -0,0 +1,22 @@
+const data = [
+  {
+    id: 1,
+    name: "Virat Kohli",
+    email: "viratkohli123@gmail.com",
+    contact: "9937775273",
+  },
+  {
+    id: 2,
+    name: "Rohit Sharma",
+    email: "rohitsharma@gmail.com",
+    contact: "9898753689",
+  },
+  {
+    id: 3,
+    name: "Surya Kumar Yadav",
+    email: "suryayadav@hotmail.com",
+    contact: "8978654783",
+  },
+];
+
+export default data;
