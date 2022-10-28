b8dbfe72939eff25fbb5339522b8e42f025393e7
Ayush Shaw <61586233+ayushshaw90@users.noreply.github.com>
2022-10-28 23:04:52 +0530

    adjusted minimum height of conference program

--- components/ConferenceProgram.jsx
+++ components/ConferenceProgram.jsx
@@ -19,14 +19,16 @@ function ConferenceProgram() {
                 <p style={{ backgroundColor: "rgba(24, 67, 83, 1)" }} className='text-white py-3 px-6 border-2 rounded-lg border-black/[0.05]'>View NITR Website</p>
             </button></Link>
         </div>
-        <div style={{ backgroundColor: "rgba(16, 44, 58, 1)" }} className='text-white text-center flex flex-col py-4 ConferenceProgram_container items-center justify-center md:w-[78%] m-auto md:rounded-xl rounded-none
+        <div className='px-2'>
+        <div style={{ backgroundColor: "rgba(16, 44, 58, 1)", minHeight: "320px" }} className='text-white text-center flex flex-col py-4 ConferenceProgram_container items-center justify-center md:w-[78%] m-auto md:rounded-xl rounded-none
         '>
-                <h1 className='text-3xl  mt-6'>CONFERENCE PROGRAM</h1>
+                <h1 className='text-2xl md:text-3xl  mt-6'>CONFERENCE PROGRAM</h1>
                 <div style={{ backgroundColor: "rgba(255, 255, 255, 0.3)" }} className=' h-1 w-1/5 my-4'></div>
-                <p className='my-2 '>TENTATIVE PROGRAM (AS ON January 15, 2022)</p>
+                <p className='my-2 mx-2'>TENTATIVE PROGRAM (AS ON January 15, 2022)</p>
                 <Link href={'/flyer.pdf'}>
                 <a style={{ backgroundColor: "rgba(24, 67, 83, 1)" }} className="ConferenceProgram_button py-2 px-5 rounded-lg mt-8 mb-7">View Program</a>
         </Link>
+        </div>
         </div>
     </div>
   )
