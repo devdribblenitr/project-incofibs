67635718712558d9dce82e595a08990b2dbc7be4
Subham Das <subhamdassneha@gmail.com>
2022-11-03 22:49:48 +0530

    gallery updated

--- components/Gallery.jsx
+++ components/Gallery.jsx
@@ -4,7 +4,23 @@ import React from "react";
 
 import styles from "../styles/gallery.module.css";
 
-const sectionOneImages = [
+const incofibs2010 = [
+  {
+    url: "./gallery/2010_1.jpg",
+    key: 1,
+  },
+  {
+    url: "./gallery/2010_2.jpg",
+    key: 2,
+  },
+  {
+    url: "./gallery/2010_3.jpg",
+    key: 3,
+  },
+ 
+];
+
+const incofibs2015 = [
   {
     url: "./gallery/1.jpg",
     key: 1,
@@ -31,29 +47,29 @@ const sectionOneImages = [
   },
 ];
 
-const sectionTwoImages = [
+const nitr = [
   {
-    url: "./gallery/1.jpg",
+    url: "./gallery/nitr_1.jpg",
     key: 1,
   },
   {
-    url: "./gallery/2.jpg",
+    url: "./gallery/nitr_2.jpg",
     key: 2,
   },
   {
-    url: "./gallery/3.jpg",
+    url: "./gallery/nitr_3.jpg",
     key: 3,
   },
   {
-    url: "./gallery/4.jpg",
+    url: "./gallery/nitr_4.jpg",
     key: 4,
   },
   {
-    url: "./gallery/5.jpg",
+    url: "./gallery/nitr_5.jpg",
     key: 5,
   },
   {
-    url: "./gallery/6.jpg",
+    url: "./gallery/nitr_6.jpg",
     key: 6,
   },
 ];
@@ -65,7 +81,7 @@ const Gallery = () => {
         <h1 className="max-w-[1440px] mx-auto text-center my-2 text-4xl text-communityColor2 font-bold">InCoFIBS 2010</h1>
         <div className="w-24 h-1 bg-communityColor2 mx-auto my-3"></div>
         <div className={styles.imageGrid}>
-          {sectionOneImages.map((image) => {
+          {incofibs2010.map((image) => {
             return (
               <div key={image.key}>
                 <img src={image.url} alt="" />
@@ -78,7 +94,20 @@ const Gallery = () => {
         <h1 className="max-w-[1440px] mx-auto text-center my-2 text-4xl text-communityColor2 font-bold">InCoFIBS 2015</h1>
         <div className="w-24 h-1 bg-communityColor2 mx-auto my-3"></div>
         <div className={styles.imageGrid}>
-          {sectionTwoImages.map((image) => {
+          {incofibs2015.map((image) => {
+            return (
+              <div key={image.key}>
+                <img src={image.url} alt="" />
+              </div>
+            );
+          })}
+        </div>
+      </section>
+      <section className={styles.section}>
+        <h1 className="max-w-[1440px] mx-auto text-center my-2 text-4xl text-communityColor2 font-bold">NIT ROURKELA</h1>
+        <div className="w-24 h-1 bg-communityColor2 mx-auto my-3"></div>
+        <div className={styles.imageGrid}>
+          {nitr.map((image) => {
             return (
               <div key={image.key}>
                 <img src={image.url} alt="" />
