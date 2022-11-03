8a93d35ac3ab1a43b97dd4b7e04562a62a4231f7
Aashirbad Sabat <aum.aashirbad@gmail.com>
2022-11-04 02:47:36 +0530

    team content updated

--- components/StudentBody/Body.jsx
+++ components/StudentBody/Body.jsx
@@ -1,27 +1,45 @@
-import Image from 'next/image'
-import React from 'react'
-import teacher from "../../public/teacher.jpg"
-import EmailIcon from '@mui/icons-material/Email';
-import PhoneIcon from '@mui/icons-material/Phone';
+import Image from "next/image";
+import React from "react";
+import teacher from "../../public/teacher.jpg";
+import EmailIcon from "@mui/icons-material/Email";
+import PhoneIcon from "@mui/icons-material/Phone";
 
-const Body = ({title,mail,Contact}) => {
-    return (
-    
-        <div class="w-full   rounded-lg border  mx-auto my-6 ">
-            <div class="flex flex-col items-center mt-0  comm:pt-6 comm:my-6 shadow-[-10px_-10px_30px_4px_rgba(0,0,0,0.1),_10px_10px_30px_4px_rgba(45,78,255,0.15)]">
-                <Image class="  border-2 rounded-lg " src={teacher} alt="Bonnie image" width="220px" height="220px"/>
-                <h5 class="my-1 text-xl font-bold text-gray-900 dark:text-[#102c3a]">{title}</h5>
-                <span class="text-md text-gray-200 dark:text-[#102c3a] font-bold ">Designation</span>
-                <div class="md:flex mt-4 space-x-4 md:mt-6 pb-6">
-                    <a href= {`mailto:${mail}`} class="inline-flex items-center py-2 sm:px-4 px-2 text-sm font-medium text-center text-white bg-orange-700 rounded-lg hover:bg-orange-600 focus:ring-4 focus:outline-none focus:ring-blue-300 "><EmailIcon style = {{color: "white"}} /> </a>
-                    
-                    <a href={`tel:${Contact}`} class="inline-flex items-center py-2 sm:px-4 px-2 text-sm font-medium text-center text-white bg-orange-700 rounded-lg hover:bg-orange-600 focus:ring-4 focus:outline-none focus:ring-blue-300"> <PhoneIcon style = {{color: "white"}} /></a>
-                </div>
-            </div>
+const Body = ({ title, mail, Contact, img, designation }) => {
+  return (
+    <div class="w-full   rounded-lg border  mx-auto my-6 ">
+      <div class="flex flex-col items-center mt-0  comm:pt-6 comm:my-6 shadow-[-10px_-10px_30px_4px_rgba(0,0,0,0.1),_10px_10px_30px_4px_rgba(45,78,255,0.15)]">
+        <Image
+          class="  border-2 rounded-lg "
+          src={img}
+          alt="Bonnie image"
+          width="220px"
+          height="220px"
+        />
+        <h5 class="my-1 text-xl font-bold text-gray-900 dark:text-[#102c3a]">
+          {title}
+        </h5>
+        <span class="text-md text-gray-200 dark:text-[#102c3a] font-bold ">
+          {designation}
+        </span>
+        <div class="md:flex mt-4 space-x-4 md:mt-6 pb-6">
+          <a
+            href={`mailto:${mail}`}
+            class="inline-flex items-center py-2 sm:px-4 px-2 text-sm font-medium text-center text-white bg-orange-700 rounded-lg hover:bg-orange-600 focus:ring-4 focus:outline-none focus:ring-blue-300 "
+          >
+            <EmailIcon style={{ color: "white" }} />{" "}
+          </a>
+
+          <a
+            href={`tel:${Contact}`}
+            class="inline-flex items-center py-2 sm:px-4 px-2 text-sm font-medium text-center text-white bg-orange-700 rounded-lg hover:bg-orange-600 focus:ring-4 focus:outline-none focus:ring-blue-300"
+          >
+            {" "}
+            <PhoneIcon style={{ color: "white" }} />
+          </a>
         </div>
-        
-            
-          )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Body
+export default Body;
--- components/StudentBody/data.js
+++ components/StudentBody/data.js
@@ -1,40 +1,86 @@
-const data=[
-    {   
-        id:1,
-        title:'Virat',
-        mail:'viratkohli123@gmail.com',
-        Contact:'9937775273',
-    },
-    {   
-        id:2,
-        title:'Rohit ',
-        mail:'rohitsharma@gmail.com',
-        Contact:'9898753689',
-    },
-    {   
-        id:3,
-        title:'Surya ',
-        mail:'suryayadav@hotmail.com',
-        Contact:'8978654783',
-    },
-    {   
-        id:4,
-        title:'Hardik ',
-        mail:'hardikpandya@yahoo.com',
-        Contact:'8796578489',
-    },
-    {   
-        id:5,
-        title:'Dinesh ',
-        mail:'dineshkarthik@gmail.com',
-        Contact:'8709857688',
-    },
-    {   
-        id:6,
-        title:'shami',
-        mail:'mohammadshami@gmail.com',
-        Contact:'9876886755'
-    }
-]  
+import Sahil from "../../public/student/Sahil.jpg";
+import venu from "../../public/student/venu.jpeg";
+import samchita from "../../public/student/Sahil.jpg";
+import parthipan from "../../public/student/parthipan.jpg";
+import sambit from "../../public/student/Sahil.jpg";
+import bibhuti from "../../public/student/bibhuti.png";
+import rinkesh from "../../public/student/Rinkesh.jpg";
+import suman from "../../public/student/Suman.jpg";
+import sneha from "../../public/student/Sneha.jpeg";
 
-export default data
+const data = [
+  {
+    id: 1,
+    designation: "Technical Lead",
+    img: Sahil,
+    title: "Mohammad Sahil",
+    mail: "viratkohli123@gmail.com",
+    Contact: "9937775273",
+  },
+  {
+    id: 2,
+    img: venu,
+    designation: "Content Lead",
+    title: "Venugopal",
+    mail: "rohitsharma@gmail.com",
+    Contact: "9898753689",
+  },
+  {
+    id: 3,
+    img: samchita,
+    designation: "Design Lead",
+    title: "Samchita",
+    mail: "suryayadav@hotmail.com",
+    Contact: "8978654783",
+  },
+  {
+    id: 4,
+    img: parthipan,
+    designation: "Outreach Lead",
+    title: "Parthipan: ",
+    mail: "hardikpandya@yahoo.com",
+    Contact: "8796578489",
+  },
+  {
+    id: 5,
+    img: sambit,
+    designation: "Sponsorships Lead",
+    title: "Sambit ",
+    mail: "dineshkarthik@gmail.com",
+    Contact: "8709857688",
+  },
+  {
+    id: 6,
+    img: bibhuti,
+    designation: "Finance Lead",
+    title: "Bibhuti",
+    mail: "mohammadshami@gmail.com",
+    Contact: "9876886755",
+  },
+  {
+    id: 7,
+    img: rinkesh,
+    designation: "Travel Lead",
+    title: "Rinkesh",
+    mail: "mohammadshami@gmail.com",
+    Contact: "9876886755",
+  },
+  {
+    id: 8,
+    img: suman,
+    designation: "Accomodation Lead",
+    title: "Suman",
+    mail: "mohammadshami@gmail.com",
+    Contact: "9876886755",
+  },
+  {
+    id: 9,
+    img: sneha,
+    designation: "Media Lead",
+    title: "Sneha",
+    mail: "mohammadshami@gmail.com",
+    Contact: "9876886755",
+  },
+];
+
+export default data;
