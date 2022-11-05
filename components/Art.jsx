428d4b3bad0984b456c138d0f9245fe6adba5efd
Anubhuti Pandey <anubhutipandey028@gmail.com>
2022-11-05 17:17:25 +0530

    added life science

--- components/Art.jsx
+++ components/Art.jsx
@@ -1,24 +1,80 @@
-import React from 'react';
+import React from "react";
 const Nitr = () => {
-    return ( <>
-
-        <div className='mb-10 flex flex-col items-center lg:my-8'>
-        <div className='mx-4 justify-center '>
-        <div className='flex flex-col mt-20 items-center  lg:items-start lg:ml-[100px]'>
-        <img className='center z-10 w-[320px] md:w-[400px] lg:absolute h-72 lg:h-96 rounded-3xl shadow-2xl' src="https://res.cloudinary.com/dzbax077h/image/upload/v1665169731/conf_r2ahdf.jpg" alt='incofibs'/>  
+  return (
+    <>
+      <div className="mb-10 flex flex-col items-center lg:my-8">
+        <div className="mx-4 justify-center ">
+          <div className="flex flex-col mt-20 items-center  lg:items-start lg:ml-[100px]">
+            <img
+              className="center z-10 w-[320px] md:w-[400px] lg:absolute h-72 lg:h-96 rounded-3xl shadow-2xl"
+              src="https://res.cloudinary.com/dzbax077h/image/upload/v1665169731/conf_r2ahdf.jpg"
+              alt="incofibs"
+            />
+          </div>
+          <div className="rounded-3xl mt-10 mb-4 justify-center lg:ml-[400px] m-2 md:m-20 lg:mb-20">
+            <div className="flex flex-col space-y-4 bg-slate-100 p-5 px-8 py-20 lg:pr-20 lg:pl-40 lg:text-justify  text-center text-xl rounded-3xl h-fit max-w-5xl shadow-xl text-[#184353dc]">
+              <div className=" bg-white font-serif text-5xl text-center text-bold">
+                State of ART
+              </div>
+              <div>
+                The 3rd International Conference on Frontiers in Biological
+                Sciences (InCoFIBS-2023) will be held in{" "}
+                <span className="text-red-600">
+                  Department of Life Science, NIT Rourkela, India, from 2nd –
+                  4th February, 2023.
+                </span>{" "}
+              </div>
+              <div>
+                InCoFIBS, an international conference organized by the
+                department of Life Science, NIT Rourkela every five years,
+                addresses the advances made in the various fields of Biological
+                Sciences. The conference aims to provide a global platform for
+                exchanging ideas and works by multiple scientists, researchers,
+                and international experts. It also offers a rostrum for young
+                researchers to showcase and share their ideas with peers working
+                in their field. These advances and efforts have unleashed many
+                global players on the platform, bringing to the fore exciting
+                and equally enthralling technologies that have led to the
+                emergence of specializations and interdisciplinary fields.
+              </div>
+              <div>
+                Research is in a unique position to confront uncertainty. The
+                dire grievances brought on by Covid-19 and the climate crisis
+                demand more than utopic gestures or hopeful technologies.
+                Challenged by the tensions of our times, Researchers are tasked
+                with applying diverse forms of embodied knowledge to craft new
+                tools for post-crisis conviviality and radical kindness. We are
+                seeking future-oriented responses to current social, cultural,
+                ecological and economic challenges through the manifold means of
+                high-level practice and reflection. The conference proposes
+                three attractors to advance understanding of new directions of
+                research and stimulate new collaborative capacities and
+                interdisciplinary scholarship.
+              </div>
+              <div>
+                The 3rd International Conference on Frontiers in Biological
+                Sciences (InCoFIBS-2023) will savor the respective
+                potentialities of digital and physical modes of co-presence,
+                manifesting in two different modes: A 40minute lecture hour
+                “long conversation” online offering a variety of successive
+                presentations throughout different time zones, and a three-day
+                gathering, providing the opportunity for focused sessions,
+                exchange and brainstorming sessions in physical presence. For
+                the “long conversation”, presenters are asked to moderate the
+                discussion of the speaker immediately following their own,
+                interweaving call and response elements of presenting. For the
+                physical presence part of the conference, researchers of all
+                disciplines are invited to meet and collaborate in sessions and
+                posters during lunch time and other scheduled time within a
+                spatially decentralized, socially-distanced and
+                Covid-hygiene-approved program
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="rounded-3xl mt-10 mb-4 justify-center lg:ml-[400px] m-2 md:m-20 lg:mb-20">
-        <div className="flex flex-col space-y-4 bg-slate-100 p-5 px-8 py-20 lg:pr-20 lg:pl-40 lg:text-justify  text-center text-xl rounded-3xl h-fit max-w-5xl shadow-xl text-[#184353dc]">
-        <div className=' bg-white font-serif text-5xl text-center text-bold'>State of ART</div><div>The 3rd International Conference on Frontiers in Biological Sciences (InCoFIBS-2023) will be held in <span className='text-red-600'>Department of Life Science, NIT Rourkela, India, from 2nd – 4th February, 2023.</span> </div>
-        <div>InCoFIBS, an international conference organized by the department of Life Science, NIT Rourkela every five years, addresses the advances made in the various fields of Biological Sciences.  The conference aims to provide a global platform for exchanging ideas and works by multiple scientists, researchers, and international experts. It also offers a rostrum for young researchers to showcase and share their ideas with peers working in their field. These advances and efforts have unleashed many global players on the platform, bringing to the fore exciting and equally enthralling technologies that have led to the emergence of specializations and interdisciplinary fields.</div>
-        <div>Research is in a unique position to confront uncertainty. The dire grievances brought on by Covid-19 and the climate crisis demand more than utopic gestures or hopeful technologies. Challenged by the tensions of our times, Researchers are tasked with applying diverse forms of embodied knowledge to craft new tools for post-crisis conviviality and radical kindness. We are seeking future-oriented responses to current social, cultural, ecological and economic challenges through the manifold means of high-level practice and reflection. The conference proposes three attractors to advance understanding of new directions of research and stimulate new collaborative capacities and interdisciplinary scholarship.</div>
-        <div>The 3rd International Conference on Frontiers in Biological Sciences (InCoFIBS-2023) will savor the respective potentialities of digital and physical modes of co-presence, manifesting in two different modes: A 40minute lecture hour “long conversation” online offering a variety of successive presentations throughout different time zones, and a three-day gathering, providing the opportunity for focused sessions, exchange and brainstorming sessions in physical presence. For the “long conversation”, presenters are asked to moderate the discussion of the speaker immediately following their own, interweaving call and response elements of presenting. For the physical presence part of the conference, researchers of all disciplines are invited to meet and collaborate in sessions and posters during lunch time and other scheduled time within a spatially decentralized, socially-distanced and Covid-hygiene-approved program</div>
-        </div>
-        </div>
-        </div>
-        </div>
-        </>
-     );
-}
+      </div>
+    </>
+  );
+};
 
 export default Nitr;
---
+++ components/Lscience.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+const Lscience = () => {
+  return (
+    <>
+      <div className="mb-10 flex flex-col items-center lg:my-8">
+        <div className="mx-4 justify-center ">
+          <div className="flex flex-col mt-20 items-center  lg:items-start lg:ml-[100px]">
+            <img
+              className="center z-10 w-[320px] md:w-[400px] lg:absolute h-72 lg:h-96 rounded-3xl shadow-2xl"
+              src="https://res.cloudinary.com/dzbax077h/image/upload/v1665169731/conf_r2ahdf.jpg"
+              alt="incofibs"
+            />
+          </div>
+          <div className="rounded-3xl mt-10 mb-4 justify-center lg:ml-[400px] m-2 md:m-20 lg:mb-20">
+            <div className="flex flex-col space-y-4 bg-slate-100 p-5 px-8 py-20 lg:pr-20 lg:pl-40 lg:text-justify  text-center text-xl rounded-3xl h-fit max-w-5xl shadow-xl text-[#184353dc]">
+              <div className=" bg-white font-serif text-5xl text-center text-bold">
+                About Life Science
+              </div>
+              <div>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse
+                incidunt repudiandae eos temporibus voluptate quibusdam animi
+                nesciunt illo consequuntur enim, aspernatur ab labore hic. Ad a
+                fugit iure sit in?
+              </div>
+              <div>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Repudiandae accusamus quisquam rem cupiditate quos a omnis fugit
+                fuga! Officiis nulla quod omnis, suscipit maiores unde.
+              </div>
+              <div>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. fugit
+                fuga! Officiis nulla quod omnis, suscipit maiores unde.
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Lscience;
