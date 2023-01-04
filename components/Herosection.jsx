b182c1751d8188c94404ef9b30a4fe4314677d9e
Mohammad Sahil <sahilmejakhas@gmail.com>
2023-01-04 10:23:56 +0530

    deadlines updated

--- components/Herosection.jsx
+++ components/Herosection.jsx
@@ -8,8 +8,11 @@ const HeroSection = () => {
       <div className="absolute CarouselCards w-full">
         <CarouselCards />
         <div className='herosection_marquee'>
-        <marquee behavior="scroll" direction="left">Last date of Abstract Submission and Registration extended till December 31st</marquee>
-      </div>
+          <marquee behavior="scroll" direction="left">Last date of Abstract Submission and Registration
+            deadline soon to be announced
+            {/* extended till December 31st */}
+          </marquee>
+        </div>
       </div>
 
       <div className="absolute z-100 lg:flex lg:flex-col lg:h-screen">
--- components/ImportantDate.jsx
+++ components/ImportantDate.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import DateCard from './DateCard'
 export default function ImportantDate() {
   return (
-    <div style={{ backgroundImage: "linear-gradient(0deg, rgba(18,55,73,0.75), rgba(18,55,73,0.75)), url(Rectangle99.png)", backgroundSize: "cover", backgroundAttachment: 'fixed'}} className="py-8 md:py-14">
-       <div className="heading aboutinco">
-          <h1 className='heading_title text-white playfair text-center'>Important Dates</h1>
-        </div>
+    <div style={{ backgroundImage: "linear-gradient(0deg, rgba(18,55,73,0.75), rgba(18,55,73,0.75)), url(Rectangle99.png)", backgroundSize: "cover", backgroundAttachment: 'fixed' }} className="py-8 md:py-14">
+      <div className="heading aboutinco">
+        <h1 className='heading_title text-white playfair text-center'>Important Dates</h1>
+      </div>
       <div className="mx-auto py-4 md:py-6">
-        <div className='w-full p-4'>
+        {/* <div className='w-full p-4'>
           <div className='flex justify-center'>
           <DateCard headline={"Abstract Submission Deadline"} date={"December 31st, 2022"} alignment={"center"}></DateCard>
           </div>
@@ -15,7 +15,16 @@ export default function ImportantDate() {
             <DateCard headline={"Registration Deadline"} date={"December 31st, 2022"} alignment="left"></DateCard>
             <DateCard headline={"Full Paper Submission Deadline"} date={"December 31st, 2022"} alignment="left" ></DateCard>
           </div>
+        </div> */}
+
+
+        <div className='' style={{ fontSize: "28px", color: "white", marginTop: "45px" }}>
+          <marquee behavior="scroll" direction="left">Last date of Abstract Submission and Registration
+            deadline soon to be announced
+            {/* extended till December 31st */}
+          </marquee>
         </div>
+
       </div>
     </div>
   )
